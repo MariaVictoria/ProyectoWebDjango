d7c62ad09c6949e4821cb7c771762f7c6601d692
MariaVictoria <123131515+MariaVictoria@users.noreply.github.com>
2024-07-31 01:45:02 -0300

    login logout

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.views.generic import View
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, authenticate
 from django.contrib.auth import login, logout
 
 # Create your views here.
@@ -29,5 +29,25 @@ def cerrar_sesion(request):
     return redirect("home")
 
 
-def login_view(request):
-    return render(request, 'autenticacion/login.html')
+def logear(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get('username')
+            contra = form.cleaned_data.get('password')
+            user = authenticate(username=nombre_usuario, password=contra)
+            if user is not None: #usuario no es nada == es algo
+                login(request, user)
+                return redirect('home')
+            else:
+                for field, errors in form.errors.items():
+                    for error in errors:
+                        messages.error(request, f"{field}: {error}")
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"Informaciín incorrecta")
+
+
+    form=AuthenticationForm
+    return render(request, 'login/login.html',{'form':form})
